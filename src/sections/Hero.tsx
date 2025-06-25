269f7b2de980a5c4fe692bda2cb8f6969ec093a6
Adith <adithpv2014@gmail.com>
2025-06-25 21:08:59 +0530

    hero section animation completed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,95 @@
+"use client";
+
+import { useEffect } from "react";
 import Image from "next/image";
+import { motion, useAnimate } from "framer-motion";
 import Button from "@/components/Button";
 import designExampleImage from "@/assets/images/design-example-1.png";
 import designExampleImage2 from "@/assets/images/design-example-2.png";
 import Pointer from "@/components/Pointer";
+import cursorYouImg from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [leftPointerScope, leftPointerAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+
+    leftPointerAnimate([
+      [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [
+        leftPointerScope.current,
+        { x: 0, y: [0, 16, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+    ]);
+    rightPointerAnimate([
+      [
+        rightPointerScope.current,
+        { opacity: 1 },
+        { duration: 0.5, delay: 1.5 },
+      ],
+      [rightPointerScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+      [rightPointerScope.current, { x: 0, y: [0, 20, 0] }, { duration: 0.5 }],
+    ]);
+  }, []);
+
   return (
-    <section className="py-24 overflow-x-clip">
+    <section
+      className="py-24 overflow-x-clip"
+      style={{ cursor: `url(${cursorYouImg.src}), auto` }}
+    >
       <div className="container relative">
-        <div className="absolute -left-32 top-16 hidden lg:block">
-          <Image src={designExampleImage} alt="design example image" />
-        </div>
-        <div className="absolute -right-64 -top-16 hidden lg:block">
-          <Image src={designExampleImage2} alt="design example image two" />
-        </div>
-        <div className="absolute left-56 top-96 hidden lg:block">
+        <motion.div
+          ref={leftDesignScope}
+          initial={{ opacity: 0, y: 100, x: -100 }}
+          className="absolute -left-32 top-16 hidden lg:block"
+          drag
+        >
+          <Image
+            src={designExampleImage}
+            alt="design example image"
+            draggable="false"
+          />
+        </motion.div>
+        <motion.div
+          ref={leftPointerScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}
+          className="absolute left-56 top-96 hidden lg:block"
+        >
           <Pointer name="Andrea" />
-        </div>
-        <div className="absolute right-80 -top-4 hidden lg:block">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100, y: 100 }}
+          ref={rightDesignScope}
+          drag
+          className="absolute -right-64 -top-16 hidden lg:block"
+        >
+          <Image
+            src={designExampleImage2}
+            alt="design example image two"
+            draggable="false"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 275, y: 100 }}
+          ref={rightPointerScope}
+          className="absolute right-80 -top-4 hidden lg:block"
+        >
           <Pointer name="Brian" color="red" />
-        </div>
+        </motion.div>
         <div className="flex justify-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
             ✨$7.5M seed round raised
